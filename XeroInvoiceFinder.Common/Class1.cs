5269104e8fd3382e63d14a21e9b89111d71ce40f
Robbie Tapping <robbie@robbietapping.com.au>
2021-07-30 10:48:24 +1000

    Add project files.

---
+++ XeroInvoiceFinder.Common/Class1.cs
@@ -0,0 +1,112 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xero.NetStandard.OAuth2.Api;
+using Xero.NetStandard.OAuth2.Model.Accounting;
+using Xero.NetStandard.OAuth2.Model.Identity;
+
+namespace XeroInvoiceFinder.Common
+{
+    public class XeroClient
+    {
+
+        private readonly string _clientid;
+        private readonly string _clientSecret;
+        public static AuthResponse AuthContext { get; set; }
+
+        public XeroClient(string clientId, string clientSecret)
+        {
+
+            _clientid = clientId;
+            _clientSecret = clientSecret;
+        }
+
+
+        public async Task<AuthResponse> Authenticate()
+        {
+
+            string code = $"{_clientid}:{_clientSecret}";
+            string authorizationCode = System.Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(code));
+
+            RestSharp.RestClient client = new RestSharp.RestClient("https://identity.xero.com/");
+            var request = new RestSharp.RestRequest("/connect/token?", RestSharp.Method.POST);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            //var jsonData = new { grant_type = "client_credentials", scope = "accounting.transactions" };
+            request.AddObject(new { grant_type = "client_credentials", scope = "accounting.transactions" });
+            //var json = Newtonsoft.Json.JsonConvert.SerializeObject(jsonData);
+            request.AddHeader("Authorization", $"Basic {authorizationCode}");
+            var response = await client.ExecuteAsync<AuthResponse>(request);
+
+
+            AuthContext = response.Data;
+            return AuthContext;
+
+
+        }
+
+
+
+        public async Task<List<Connection>> GetConnection(string accessToken = null)
+        {
+            if (accessToken == null)
+            {
+                if (AuthContext == null)
+                {
+                    await Authenticate();
+                }
+                accessToken = AuthContext.access_token;
+            }
+            var apiInstance = new IdentityApi();
+
+            try
+            {
+                var result = await apiInstance.GetConnectionsAsync(accessToken);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception when calling GetConnection: " + e.Message);
+                return null;
+            }
+        }
+
+        public async Task<Invoices> GetInvoice(string xeroTenantId, string invoiceNo, string accessToken = null)
+        {
+            if (accessToken == null)
+            {
+                if (AuthContext == null)
+                {
+                    await Authenticate();
+                }
+                accessToken = AuthContext.access_token;
+            }
+            var apiInstance = new AccountingApi();
+
+            try
+            {
+                var result = await apiInstance.GetInvoicesAsync(accessToken,xeroTenantId, invoiceNumbers: new List<string>() { invoiceNo });
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception when calling GetInvoice: " + e.Message);
+                return null;
+            }
+        }
+
+
+
+    }
+
+
+
+    public class AuthResponse
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+        public string scope { get; set; }
+    }
+
+}
---
+++ XeroInvoiceFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace XeroInvoiceFinder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            GetData();
+            var x = Console.ReadLine();
+        }
+
+
+        public static async void GetData()
+        {
+            XeroInvoiceFinder.Common.XeroClient client = new Common.XeroClient("04B394EAEE844D588260B2E569F2B6A5", "6iDgnnVlyOCtEmqLqmTawcCZIS7i9BqBx7Y5kCUks5z7n4ff");
+           
+            var authContext =  await client.Authenticate();
+
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(authContext));
+
+            var connection = await client.GetConnection(authContext.access_token);
+
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(connection));
+
+            var invoice = await client.GetInvoice(connection[0].TenantId.ToString(), "ORC0999", authContext.access_token);
+            
+            Console.WriteLine(invoice._Invoices[0].AmountDue);
+
+        }
+    }
+}
